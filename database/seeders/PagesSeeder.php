a4b5b719d780da65a37337c6b207f8297940621e
Abraham Castro <cucharadepalo@users.noreply.github.com>
2025-06-01 19:51:50 +0200

    reorganización de páginas

---
+++ database/seeders/PagesSeeder.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Page;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class PagesSeeder extends Seeder
+{
+	/**
+	 * Run the database seeds.
+	 */
+	public function run(): void
+	{
+		Page::create([
+			'title' => 'Vendaval',
+			'slug' => 'home',
+			'type' => 'system',
+			'content' => 'Este contenido se mostrará en la página de inicio.',
+			'is_published' => true,
+			'in_menu' => true,
+		]);
+
+		Page::create([
+			'title' => 'Programa',
+			'slug' => 'programa',
+			'type' => 'system',
+			'content' => 'Este contenido se mostrará al final del programa.
+
+Se puede poner cualquier información relevante al programa aqui.',
+			'is_published' => true,
+			'in_menu' => true,
+		]);
+
+		Page::create([
+			'title' => 'Filmes',
+			'slug' => 'filmes',
+			'type' => 'system',
+			'content' => 'Este contenido se mostrará al principio de la página de filmes.',
+			'is_published' => true,
+			'in_menu' => true,
+		]);
+
+		Page::create([
+			'title' => 'Ribeira Sacra',
+			'slug' => 'ribeira-sacra',
+			'type' => 'system',
+			'content' => 'Este contenido se mostrará al principio de la página de filmes.',
+			'is_published' => true,
+			'in_menu' => true,
+		]);
+
+	}
+}
