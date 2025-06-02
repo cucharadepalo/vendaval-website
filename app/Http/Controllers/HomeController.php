fcffab29e8fec5a83cc7d829f8222223125ddb57
Abraham Castro <cucharadepalo@users.noreply.github.com>
2025-06-02 10:28:50 +0200

    Contenido página home

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Edition;
+use App\Models\Page;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -21,6 +22,8 @@ class HomeController extends Controller
 	 */
 	public function __invoke(Request $request)
 	{
-		return view('home');
+		$page = Page::whereSlug('home')->first();
+
+		return view('home', compact('page'));
 	}
 }
