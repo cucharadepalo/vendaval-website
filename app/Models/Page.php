e33d12ff80e9ba6ceb5b2c6dbc9f8f797283551d
Abraham Castro <cucharadepalo@users.noreply.github.com>
2025-06-02 10:58:10 +0200

    Media library en modelo de página

--- app/Models/Page.php
+++ app/Models/Page.php
@@ -3,8 +3,30 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Spatie\MediaLibrary\HasMedia;
+use Spatie\MediaLibrary\InteractsWithMedia;
+use Spatie\MediaLibrary\MediaCollections\Models\Media;
 
-class Page extends Model
+class Page extends Model implements HasMedia
 {
-    //
+	use InteractsWithMedia, SoftDeletes;
+
+	protected $fillable = [
+		'title',
+		'slug',
+		'type',
+		'content',
+		'is_published',
+		'in_menu'
+	];
+
+	/**
+	 * Spatie Media Library collections.
+	 */
+	public function registerMediaCollections(): void
+	{
+		$this->addMediaCollection('images')
+			->acceptsMimeTypes(['image/jpeg', 'image/svg+xml', 'image/png', 'image/apng', 'image/jp2', 'image/gif', 'image/webp']);
+	}
 }
