c92cb3cb9e66c834ede3e89fcdc5c1026f33596e
Abraham Castro <cucharadepalo@users.noreply.github.com>
2025-06-05 18:41:15 +0200

    Maquetación XL del programa

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -9,7 +9,7 @@
 		third-line="Ribeira Sacra"
 		:title="$edition->title" />
 
-	<main>
+	<main class="pb-24">
 		<section id="home" class="my-12 px-6 md:px-16 xl:px-20 xl:my-16">
 			@if ($page->content)
 				<div class="md-wrapper lg:text-lg lg:columns-2 lg:gap-16 lg:my-8">
