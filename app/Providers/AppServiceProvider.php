bd74e72868e11559ab4722d7da7ec1065e4b3be8
Abraham Castro <cucharadepalo@gmail.com>
2025-06-15 22:19:34 +0100

    probando enlaces simbolicos de storage

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -15,7 +15,7 @@ class AppServiceProvider extends ServiceProvider
 	public function register(): void
 	{
 		$this->app->bind('path.public', function() {
-			return realpath(base_path('www'));
+			return realpath(base_path() . DIRECTORY_SEPARATOR . 'www');
 		});
 	}
 
