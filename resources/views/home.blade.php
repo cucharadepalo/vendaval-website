b93c17bba1b7c03878ca7ad1e14937bd40cab151
Abraham Castro <cucharadepalo@gmail.com>
2025-06-15 19:24:46 +0100

    vistas sin edición

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -21,11 +21,6 @@
 		</section>
 	</main>
 @else
-<main class="min-h-dvh flex flex-col items-center justify-center">
-	<h1 class="splash-title mega">Vendaval</h1>
-	<p class="splash-subtitle simple">Mostra de Cinema Portugués</p>
-	<p class="splash-subtitle simple">Ribeira Sacra</p>
-	<p class="text-3xl my-16">Próximamente...</p>
-</main>
+@include('inactive-chunk')
 @endif
 @endsection
