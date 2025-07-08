335968f17a70fee7daf39547b521b80519966a4c
Abraham Castro <cucharadepalo@users.noreply.github.com>
2025-07-08 20:29:46 +0200

    Maquetación página espazos

--- resources/views/where.blade.php
+++ resources/views/where.blade.php
@@ -15,35 +15,37 @@
 			@endif
 			<div class="text-pretty my-12">
 				@foreach ($venues as $venue)
-					<div class="mt-12">
-						<h2 class="text-xl font-semibold">{{ $venue->name }}</h2>
-						<p class="text-lg">{{ $venue->town }}</p>
-						<ul class="text-sm mt-2 space-y-1">
-							@if ($venue->address)
-								<li>
-									<x-filament::icon icon="bx-map-alt" class="w-4 h-4 inline-block mr-1 align-text-bottom"/>
-									{{ $venue->address }}
-								</li>
-							@endif
-							@if ($venue->map)
-								<li>
-									<a href="{{ $venue->map }}" target="_blank">
-										<x-filament::icon icon="bx-map" class="w-4 h-4 inline-block mr-1 align-text-bottom"/>
-										Mapa
-									</a>
-								</li>
-							@endif
-							@if ($venue->website)
-								<li>
-									<a href="{{ $venue->website }}" target="_blank">
-										<x-filament::icon icon="bx-link-alt" class="w-4 h-4 inline-block mr-1 align-text-bottom"/>
-										{{ preg_replace(["(^https?://)", "(^http?://)"], '', $venue->website) }}
-									</a>
-								</li>
-							@endif
-						</ul>
+					<div class="mt-12 lg:grid lg:grid-cols-3 lg:col-span-8 xl:col-span-12">
+						<div>
+							<h2 class="text-xl font-semibold lg:text-2xl xl:text-3xl">{{ $venue->name }}</h2>
+							<p class="text-lg lg:text-xl">{{ $venue->town }}</p>
+							<ul class="text-sm mt-2 space-y-1 lg:text-base xl:text-lg">
+								@if ($venue->address)
+									<li>
+										<x-filament::icon icon="bx-map-alt" class="w-4 h-4 inline-block mr-1 align-text-bottom xl:w-6 xl:h-6"/>
+										{{ $venue->address }}
+									</li>
+								@endif
+								@if ($venue->map)
+									<li>
+										<a href="{{ $venue->map }}" target="_blank">
+											<x-filament::icon icon="bx-map" class="w-4 h-4 inline-block mr-1 align-text-bottom xl:w-6 xl:h-6"/>
+											Mapa
+										</a>
+									</li>
+								@endif
+								@if ($venue->website)
+									<li>
+										<a href="{{ $venue->website }}" target="_blank">
+											<x-filament::icon icon="bx-link-alt" class="w-4 h-4 inline-block mr-1 align-text-bottom xl:w-6 xl:h-6"/>
+											{{ preg_replace(["(^https?://)", "(^http?://)"], '', $venue->website) }}
+										</a>
+									</li>
+								@endif
+							</ul>
+						</div>
 						@if ($venue->text)
-							<x-content-wrapper :content="$venue->text" class="lg:text-lg xl:text-xl" />
+							<x-content-wrapper :content="$venue->text" class="mt-6 lg:mt-0 lg:col-span-2 lg:text-lg xl:text-xl" />
 						@endif
 					</div>
 				@endforeach
