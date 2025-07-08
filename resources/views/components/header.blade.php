bf917cf3b88dfab55f161c8226782debdcaf7c74
Abraham Castro <cucharadepalo@users.noreply.github.com>
2025-07-08 21:14:07 +0200

    Enlaces a instagram y facebook en el menu

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -4,24 +4,36 @@
 
 		<div>
 			<h1><a href="{{ route('home') }}" class="splash-title {{ $height == 'tall' ? 'mega' : 'simple' }}">Vendaval</a> <span class="splash-subtitle {{ $height == 'tall' ? 'simple' : 'mega' }}">{{ $title }}</span> @isset($thirdLine)<span class="splash-subtitle simple">{{ $thirdLine }}</span>@endisset</h1>
-		<div>
+		</div>
 
-		<nav class="site-menu absolute w-full h-full -top-full right-0 bottom-0 left-0 bg-(--vdl-splash-bg-color) text-(--vdl-splash-txt-color) flex items-center justify-center z-10">
-
-			<ul class="text-4xl font-brand tracking-wider text-center lg:flex lg:justify-center lg:gap-8 lg:text-5xl">
-				@foreach ($pages as $page)
-				<li><a href="{{ $page->getLink() }}">{{ $page->title }}</a></li>
-				@endforeach
-		</nav>
-
-		<button type="button" class="toggle-menu w-10 h-10 absolute top-6 right-6 bg-(--vdl-splash-bg-color) text-(--vdl-splash-txt-color) flex items-center justify-center z-20">
-			<span class="state-close">
-				<x-filament::icon icon="bx-menu" class="w-8 h-8 block" />
-			</span>
-			<span class="state-open hidden">
-				<span class="text-4xl leading-6">&times;</span>
-				<span class="sr-only">Pechar</span>
-			</span>
-		</button>
 	</div>
+
+	<nav class="site-menu absolute w-full h-full -top-full right-0 bottom-0 left-0 bg-(--vdl-splash-bg-color) text-(--vdl-splash-txt-color) flex items-center justify-center z-10">
+
+		<ul class="text-4xl font-brand tracking-wider text-center lg:flex lg:justify-center lg:gap-8 lg:text-5xl">
+			@foreach ($pages as $page)
+			<li><a href="{{ $page->getLink() }}">{{ $page->title }}</a></li>
+			@endforeach
+		</ul>
+
+		<ul class="absolute bottom-2 right-2 lg:right-8 lg:bottom-4 text-center flex gap-1">
+			<li>
+				<a href="https://www.instagram.com/vendaval_mostracinemapt/" target="_blank" title="Instagram"><x-filament::icon icon="bxl-instagram" class="w-8 h-8 inline-block" /></a>
+			</li>
+			<li>
+				<a href="https://www.facebook.com/vendavalmostracinemapt" target="_blank" title="Facebook"><x-filament::icon icon="bxl-facebook" class="w-8 h-8 inline-block" /></a>
+			</li>
+		</ul>
+	</nav>
+
+	<button type="button" class="toggle-menu w-10 h-10 absolute top-6 right-6 bg-(--vdl-splash-bg-color) text-(--vdl-splash-txt-color) flex items-center justify-center z-20">
+		<span class="state-close">
+			<x-filament::icon icon="bx-menu" class="w-8 h-8 block" />
+		</span>
+		<span class="state-open hidden">
+			<span class="text-4xl leading-6">&times;</span>
+			<span class="sr-only">Pechar</span>
+		</span>
+	</button>
+
 </header>
