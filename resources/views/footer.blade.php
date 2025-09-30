d1bef5615cef083724610a11fafb41f22a157c71
Abraham Castro <cucharadepalo@users.noreply.github.com>
2025-09-30 11:32:17 +0200

    ajustes posición

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -1,5 +1,5 @@
 <footer class="w-full min-h-4 bg-(--vdl-splash-bg-color) text-(--vdl-splash-txt-color) text-sm">
-	<div class="px-6 py-12 grid grid-cols-1 gap-6 items-start md:px-16 xl:px-20">
+	<div class="px-6 py-12 flex flex-wrap items-start gap-6 md:px-16 xl:px-20">
 		@isset($edition)
 			@isset($edition->footer)
 				{!! str($edition->footer)->markdown()->sanitizeHtml() !!}
@@ -15,11 +15,11 @@
 			<div class="space-y-2">
 				<p class="text-orange-300">Financiado por</p>
 				<div class="flex flex-wrap items-center justify-start gap-6">
-					<img src="{{ asset('images/logos/deputacion-lugo.png') }}" alt="Logo Deputación de Lugo" loading="lazy" width="185" height="50">
+					<img src="{{ asset('images/logos/deputacion-lugo.png') }}" alt="Logo Deputación de Lugo" loading="lazy" width="167" height="45">
 					<img src="{{ asset('images/logos/ICA.png') }}" alt="Instituto do cinema e do audiovisual" loading="lazy" width="180" height="41">
 					<img src="{{ asset('images/logos/republica-portuguesa.png') }}" alt="República Portuguesa. Cultura Juventude e desporto" loading="lazy" width="123" height="64">
 					<img src="{{ asset('images/logos/camoes.png') }}" alt="Logo Instituto Camoes" loading="lazy" width="120" height="60">
-					<img src="{{ asset('images/logos/sober-logo.svg') }}" alt="Sober" loading="lazy" width="93" height="40">
+					<img src="{{ asset('images/logos/sober-logo.svg') }}" alt="Sober" loading="lazy" width="84" height="36">
 				</div>
 			</div>
 			<div class="space-y-2">
