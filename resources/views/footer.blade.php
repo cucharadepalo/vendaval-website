c6243eeea0bdc10ecb455805412dc261b0eedc53
Abraham Castro <cucharadepalo@users.noreply.github.com>
2025-09-30 11:23:29 +0200

    Logos añadidos y editados

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -1,31 +1,34 @@
 <footer class="w-full min-h-4 bg-(--vdl-splash-bg-color) text-(--vdl-splash-txt-color) text-sm">
-	<div class="px-6 py-12 grid grid-cols-2 gap-6 items-center md:px-16 md:grid-cols-3 md:gap-12 lg:grid-cols-6 lg:items-start xl:px-20">
+	<div class="px-6 py-12 grid grid-cols-1 gap-6 items-start md:px-16 xl:px-20">
 		@isset($edition)
 			@isset($edition->footer)
 				{!! str($edition->footer)->markdown()->sanitizeHtml() !!}
 			@endisset
 			@empty($edition->footer)
-			<div class="space-y-4">
-				<p>Promovido por</p>
-				<img src="{{ asset('images/logos/memoria-e-naufraga.png') }}" alt="Logo a Memoria é naufraga">
+			<div class="space-y-2">
+				<p class="text-orange-300">Promovido por</p>
+				<div class="flex items-end justify-start gap-6">
+					<img src="{{ asset('images/logos/memoria-e-naufraga.png') }}" alt="Logo a Memoria é naufraga" loading="lazy" width="120" height="76">
+					<img src="{{ asset('images/logos/duplacena.png') }}" alt="Duplacena" loading="lazy" width="180" height="23">
+				</div>
 			</div>
-			<div class="space-y-4">
-				<p>Financiado por</p>
-				<img src="{{ asset('images/logos/deputacion-lugo.png') }}" alt="Logo Deputación de Lugo">
+			<div class="space-y-2">
+				<p class="text-orange-300">Financiado por</p>
+				<div class="flex flex-wrap items-center justify-start gap-6">
+					<img src="{{ asset('images/logos/deputacion-lugo.png') }}" alt="Logo Deputación de Lugo" loading="lazy" width="185" height="50">
+					<img src="{{ asset('images/logos/ICA.png') }}" alt="Instituto do cinema e do audiovisual" loading="lazy" width="180" height="41">
+					<img src="{{ asset('images/logos/republica-portuguesa.png') }}" alt="República Portuguesa. Cultura Juventude e desporto" loading="lazy" width="123" height="64">
+					<img src="{{ asset('images/logos/camoes.png') }}" alt="Logo Instituto Camoes" loading="lazy" width="120" height="60">
+					<img src="{{ asset('images/logos/sober-logo.svg') }}" alt="Sober" loading="lazy" width="93" height="40">
+				</div>
 			</div>
-			<div class="space-y-4">
-				<img src="{{ asset('images/logos/camoes.png') }}" alt="Logo Instituto Camoes">
+			<div class="space-y-2">
+				<p class="text-orange-300">Colabora</p>
+				<img src="{{ asset('images/logos/concello-de-boveda-logo.svg') }}" alt="Concello de Bóveda" loading="lazy" width="103" height="50">
 			</div>
-			<div class="space-y-4">
-				<img src="{{ asset('images/logos/sober-logo.svg') }}" alt="Sober">
-			</div>
-			<div class="space-y-4">
-				<p>Colabora</p>
-				<img src="{{ asset('images/logos/concello-de-boveda-logo.svg') }}" alt="Concello de Bóveda">
-			</div>
-			<div class="space-y-4">
-				<p>Espazo</p>
-				<img src="{{ asset('images/logos/a-casa-da-memoria.png') }}" alt="A Casa de Memoria">
+			<div class="space-y-2">
+				<p class="text-orange-300">Espazo</p>
+				<img src="{{ asset('images/logos/a-casa-da-memoria.png') }}" alt="A Casa de Memoria" loading="lazy" width="100" height="76">
 			</div>
 			@endempty
 		@endisset
